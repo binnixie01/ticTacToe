adc31cc611620f8b6fd42adb5533a4faa05a51d3
Thangjam Binson <binsonsingh@gmail.com>
2023-09-23 22:42:28 +0530

    player mark done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let arr = new Array(9);
-arr=['X','O']
+const arr = new Array(9);
+// arr=[]
 
 // const player = (name) => {
 //   let button = Array.from(document.getElementsByClassName("but"));
@@ -12,11 +12,47 @@ arr=['X','O']
 // };
 // const playerX=player('X');
 // playerX.mark()
-  let button = Array.from(document.getElementsByClassName("but"));
 // button.forEach((element)=>console.log())
-for(let i=0;i<button.length;i++){
-    button[i].textContent=arr[i];
+let button = Array.from(document.getElementsByClassName("but"));
+const showMarkers=(()=>{
+   function show(){for(let i=0;i<button.length;i++){
+        button[i].textContent=arr[i];
+
+    }}
+    return {show}
+})()
+
+const player =(name,index)=>{
+    function addArray(index){
+        arr.splice(index,0,name)
+
+    }
+    return {addArray}
+    
+
+
 }
 
-// button.forEach((element) =>element.addEventListener("click", () => arr.push('X')))
+const play =(()=>{
+  function change (){for(let i=0;i<button.length;i++){
+        button[i].addEventListener('click',gameLogic)
+        console.log(i)
+    }
+    
+    function gameLogic(i){
+        const playerX=player('X',i)
+        playerX.addArray(i)
+        showMarkers.show()
+    
+    }}
+    return{change}
+
+})()
+play.change()
+
+
+
+
+
+// button.forEach((element) =>element.adgdEventListener("click", () => arr.push('X')))
 
