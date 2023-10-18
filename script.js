390bb7b8c058c7882122a6f46c9abb416860b8fc
Thangjam Binson <binsonsingh@gmail.com>
2023-10-18 22:36:30 +0530

    added checkWin

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
 const arr = [];
-let k=0;
-// arr=[]
+let k = 0;
 
-// const player = (name) => {
-//   let button = Array.from(document.getElementsByClassName("but"));
-//   const mark=()=>button.forEach((element) =>
-//     element.addEventListener("click", () => (element.textContent = `${name
-    
-//     }`))
-//   );
-//   return {mark};
-// };
-// const playerX=player('X');
-// playerX.mark()
-// button.forEach((element)=>console.log())
-let button = Array.from(document.getElementsByClassName("but"));
-const showMarkers=(()=>{
-   function show(){for(let i=0;i<button.length;i++){
-        button[i].textContent=arr[i];
+let button = Array.from(document.getElementsByClassName("but")); //button selection
 
-    }}
-    return {show}
-})()
-
-const player =(name,index)=>{
-    function addArray(){
-        arr[index]=name;
-
+// iife for showing markings by players
+const showMarkers = (() => {
+  function show() {
+    for (let i = 0; i < button.length; i++) {
+      button[i].textContent = arr[i];
     }
-    return {addArray}
-    
+  }
+  return { show };
+})();
 
+// player object name for x or y ...index for adding x or y in array for showmarker()
+const player = (name, index) => {
+  function addArray() {
+    arr[index] = name;
+  }
+  return { addArray };
+};
 
-}
-
-const play =(()=>{
-  function change (){for(let j=0;j<button.length;j++){
-        button[j].addEventListener('click',function(){
-            
-            if(arr[j]==undefined){
-            if(k%2==0){
-                const playerX=player('X',j)
-                playerX.addArray()
-                k++;
-                
-            }else{
-                const playerX=player('O',j)
-                playerX.addArray()
-                k++;
-
-            }}
-            console.log('s'+j)
-            showMarkers.show()
-            console.log(k)
-            
-        })
-
+//game flow
+const play = (() => {
+  function change() {
+    for (let j = 0; j < button.length; j++) {
+      button[j].addEventListener("click", function () {
+        if (!arr[j]) {
+          if (k % 2 == 0) {
+            //k for player playing order and no remarking
+            const playerX = player("X", j);
+            playerX.addArray();
+            k++;
+          } else {
+            const playerX = player("O", j);
+            playerX.addArray();
+            k++;
+          }
+        }
+        showMarkers.show();
+        checkWin()
+      });
     }
-    
+  }
+  return { change };
+})(); //iife
+play.change();
+
+//winner loser logic
+const checkWin = () => {
+  const winner = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winner.forEach((e) => {
+    if (arr[e[0]] === arr[e[1]] && arr[e[2]] === arr[e[1]] && arr[e[0]]!==undefined) {
+      alert(`winner is ${arr[e[0]]}`);
+      clear()
     }
-    return{change}
+  });
+};
 
-})()
-play.change()
+//play game again...  clearing the prev marks
+const again = document.getElementById("again");
+again.addEventListener("click", clear);
 
-
-const again = document.getElementById('again');
-again.addEventListener('click',function(){arr.length=0;
-showMarkers.show()
-})
-
-
-// button.forEach((element) =>element.adgdEventListener("click", () => arr.push('X')))
-
+function clear() {
+    arr.length = 0;
+    k = 0;
+    showMarkers.show();
+  }
